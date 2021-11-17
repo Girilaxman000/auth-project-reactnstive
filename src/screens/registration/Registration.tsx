034003726de99eb80448783cbcfe625f1e652711
Laxman Giri <laxman.giri@convergetree.com>
2021-11-17 11:46:44 +0545

    add lint script in package.json file

--- src/screens/registration/Registration.tsx
+++ src/screens/registration/Registration.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { View, Text, TextInput } from 'react-native'
 import { CheckBox } from "react-native-elements";
-
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import * as RootNavigation from "../../navigation/RootNavigation";
